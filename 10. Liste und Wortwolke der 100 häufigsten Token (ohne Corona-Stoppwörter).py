a8ae5edf38c095e1f34d366a4bf6958a14979dcd
KittyKat808 <134396609+KittyKat808@users.noreply.github.com>
2025-12-19 11:39:19 +0100

    Update 10. Liste und Wortwolke der 100 häufigsten Token (ohne Corona-Stoppwörter).py

--- 10. Liste und Wortwolke der 100 häufigsten Token (ohne Corona-Stoppwörter).py
+++ 10. Liste und Wortwolke der 100 häufigsten Token (ohne Corona-Stoppwörter).py
@@ -109,6 +109,7 @@ def analyze_tokens(tweets, corona_stopwords, output_dir):
 
 
 def main():
+    # An eigene Pfade anpassen!
     input_file = r"C:\Users\[NUTZERNAME]\[ORDNERNAME]\Cleaned_Data.jsonl"
     stopwords_file = r"C:\Users\[NUTZERNAME]\[ORDNERNAME]\corona_stopwords.txt"
     output_dir = r"C:\Users\[NUTZERNAME]\[ORDNERNAME]\Tokens"
@@ -119,4 +120,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
